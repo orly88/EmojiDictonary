045af158109cf79eaf8deba864e9f39ae2e806cb
Orly Claudio Gómez Ramírez <orly@MacBook-Pro-de-Orly.local>
2017-05-07 21:59:50 -0500

    Initial Commit

---
+++ EmojiDictonary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiDictonary
+//
+//  Created by Orly Claudio Gómez Ramírez on 07/05/17.
+//  Copyright © 2017 Orly Claudio Gómez Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
